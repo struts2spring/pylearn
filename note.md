6fbbdb292c8e791412e6f2a53834d6872b5b485b
struts2spring <struts2spring@gmail.com>
2014-10-13 17:15:22 +0530

    Update and rename note.md to note.py

--- note.md
+++
@@ -1,2 +0,0 @@
-sudo add-apt-repository -y ppa:appgrid/stable
-sudo apt-get update && sudo apt-get install appgrid
---
+++ note.py
@@ -0,0 +1,5 @@
+sudo add-apt-repository -y ppa:appgrid/stable
+sudo apt-get update && sudo apt-get install appgrid
+
+sudo add-apt-repository ppa:yorba/daily-builds
+sudo apt-get update && sudo apt-get install california
